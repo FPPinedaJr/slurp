a4d32a0dbd3f823fbbfe8631b509a882f01dacf7
fppinedajr <fernandojr03.pineda@gmail.com>
2025-05-16 09:59:51 +0800

    Setup initial project structure

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/output.css?v=0.1">
+    <link rel="stylesheet" href="./assets/css/fontawesome/all.min.css">
+    <link rel="stylesheet" href="./assets/css/fontawesome/fontawesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <script src="./assets/js/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lozad"></script>
+    <title>Document</title>
+</head>
+
+
+<body class="flex flex-col items-center w-screen min-h-screen overflow-x-hidden bg-slate-100">
+    <main class="flex flex-col items-center flex-grow w-full h-full mb-10">
+
+    </main>
+</body>
+
+
+<script>
+</script>
+
+</html>
