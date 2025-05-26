7a04eb212bd3583317bddbef6399137921207f18
fppinedajr <fernandojr03.pineda@gmail.com>
2025-05-27 07:42:49 +0800

    Add prod db

--- public/includes/connect_db.php
+++ public/includes/connect_db.php
@@ -1,45 +1,52 @@
 <?php
-function loadEnv($path)
-{
-	if (!file_exists($path)) {
-		throw new Exception("Environment file not found.");
-	}
+// function loadEnv($path)
+// {
+// 	if (!file_exists($path)) {
+// 		throw new Exception("Environment file not found.");
+// 	}
 
-	$lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	foreach ($lines as $line) {
-		if (strpos(trim($line), '#') === 0) {
-			continue;
-		}
+// 	$lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+// 	foreach ($lines as $line) {
+// 		if (strpos(trim($line), '#') === 0) {
+// 			continue;
+// 		}
 
-		list($key, $value) = explode('=', $line, 2);
-		putenv("$key=$value");
-	}
-}
-
-
-
-
-// loadEnv('/home/u273960544/domains/palawansu-usg.com/public_html/palawansu-usg/usg_tiniguiban.env');  // ----- PRODUCTION   ----- //
-loadEnv(__DIR__ . '/../.env');                          // ----- DEVELOPMENT  ----- //
+// 		list($key, $value) = explode('=', $line, 2);
+// 		putenv("$key=$value");
+// 	}
+// }
 
 
 
-date_default_timezone_set("Asia/Manila");
-$charset = 'utf8mb4';
+
+// // loadEnv('/home/u273960544/domains/palawansu-usg.com/public_html/palawansu-usg/usg_tiniguiban.env');  // ----- PRODUCTION   ----- //
+// loadEnv(__DIR__ . '/../.env');                          // ----- DEVELOPMENT  ----- //
 
 
-$hostname = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
-$defaultSchema = getenv('DB_NAME');
-$charset = getenv('DB_CHARSET');
+
+// date_default_timezone_set("Asia/Manila");
+// $charset = 'utf8mb4';
+
+
+// $hostname = getenv('DB_HOST');
+// $port = getenv('DB_PORT');
+// $username = getenv('DB_USER');
+// $password = getenv('DB_PASS');
+// $defaultSchema = getenv('DB_NAME');
+// $charset = getenv('DB_CHARSET');
 
 $hostname = "localhost";
 $port = "3306";
 $username = "root";
 $password = "";
 $defaultSchema = "slurp";
+$charset = "utf8mb4";
+
+$hostname = "77.37.35.51";
+$port = "3306";
+$username = "u273960544_kisser";
+$password = "h1g0pM4l4l4*";
+$defaultSchema = "u273960544_slurp";
 $charset = "utf8mb4";
 
 $dsn = "mysql:host=$hostname;dbname=$defaultSchema;charset=$charset;port=$port";
